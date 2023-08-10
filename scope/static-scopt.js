91f37638f0bfa1997b3558b2ab96ea5a72a2f994
zgy3234748729 <3234748729@qq.com>
2023-08-11 07:26:13 +0800

    feat:静态作用与动态作用域的区别

--- scope/static-scopt.js
+++ scope/static-scopt.js
@@ -4,8 +4,8 @@
 //3）js中没有动态作用域
 
 
-//所有定义的变量在binding阶段，按照词法作用域进行(编译阶段)
-//动态作用域，变量的binding，在执行阶段才进行
+// 所有定义的变量在binding阶段，按照词法作用域进行(编译阶段)
+// 动态作用域，变量的binding，在执行阶段才进行
 let x = 10
 
 function test() {
@@ -15,11 +15,16 @@ function test() {
 
 //静态作用域binding的是变量，和变量的值无关
 //变量的值需要在调用时才能知道是什么
-
+x = 1
 test ()
 
+/*
+ *
+ */
+
+
 function main() {
-    x = 20
+    let x = 20
     test()
 }
 main()
