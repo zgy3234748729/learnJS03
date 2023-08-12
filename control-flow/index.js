5537329a4e0f07092968e557f1613f7760961b06
zgy3234748729 <3234748729@qq.com>
2023-08-12 15:09:27 +0800

    feat:index.js

---
+++ control-flow/index.js
@@ -0,0 +1,13 @@
+console.log(!false)
+console.log(true && false)
+console.log(true || false)
+
+
+const name = 'jyt'
+if (name === 'zgy') {
+    console.log('is ZGY')
+} else if (name === 'jyt') {
+    console.log('is JYT')
+} else {
+    console.log('is other')
+}
