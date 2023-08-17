303b0d0f0c402922aefc0cec175e9abebe4ce0bc
zgy3234748729 <3234748729@qq.com>
2023-08-17 09:00:13 +0800

    feat:回调地狱

--- function/callback-hell.js
+++ function/callback-hell.js
@@ -1,17 +1,23 @@
 function network(url, fn) {
-    // 请求url， 通过fn返回url结果
-    fn({data: url})
+  // 请求url， 通过fn返回url结果
+  fn({ data: url });
 }
 
 // 当回调函数有依赖关系时，会出现回调地狱
-const fn = function(params) {
-    console.log(params)
-    network(`${params.data}|second`, function(params) {
-        console.log(params)
-        network(`${params.data}|third`, function(params) {
-            console.log(params)
-        })
-    })
-}
+const fn = function (params) {
+  console.log(params);
+  network(`${params.data}|second`, function (params) {
+    console.log(params);
+    network(`${params.data}|third`, function (params) {
+      console.log(params);
+      network(`${params.data}|fourth`, function (params) {
+        console.log(params);
+        network(`${params.data}|fifth`, function (params) {
+          console.log(params);
+        });
+      });
+    });
+  });
+};
 
-network('first', fn)
+network("first", fn);
